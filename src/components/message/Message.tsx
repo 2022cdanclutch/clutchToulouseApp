22d480d659c1b1e9e0f71966c3d9f467b7dc184e
cmoynard <cmoynard.etude@gmail.com>
2023-01-16 13:59:51 +0100

    feat: delete button on favorites

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -1,57 +1,82 @@
-import { MessageStyle, MessageContentStyle, MessageTimestampLeft } from './message.style'
-import Ievent from '../../redux/slices/Ievent'
-import { format, getHours } from 'date-fns'
-import { Text } from 'react-native'
-import fr from 'date-fns/locale/fr'
-import { capitalize } from 'lodash'
-import { FC } from 'react'
-import { FontAwesome5 } from '@expo/vector-icons'
-import { useAppSelector } from '../../redux/hooks'
+import {
+  MessageStyle,
+  MessageContentStyle,
+  MessageTimestampLeft,
+} from "./message.style";
+import Ievent from "../../redux/slices/Ievent";
+import { format, getHours } from "date-fns";
+import { Text, Button, StyleSheet, Pressable } from "react-native";
+import fr from "date-fns/locale/fr";
+import { capitalize } from "lodash";
+import { FC } from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 
 type TMessage = {
-    event: Ievent,
-    index: number,
-}
+  event: Ievent;
+  index: number;
+};
 const Message: FC<TMessage> = ({ event, index }) => {
-  const alerts = useAppSelector(state => state.alerts.alerts)
-  console.log('ALERTS', alerts)
-  const whenEventIs = (date) => {
-    const hours = getHours(new Date(date))
+  var alerts = useAppSelector((state) => state.alerts.alerts);
+  console.log("ALERTS", alerts);
+  const whenEventIs = (date: any) => {
+    const hours = getHours(new Date(date));
     if (hours < 18 && hours > 12) {
-      return 'Cette après-midi'
+      return "Cette après-midi";
     } else if (hours < 12) {
-      return 'Ce matin'
+      return "Ce matin";
     } else {
-      return 'Ce soir'
+      return "Ce soir";
     }
-  }
+  };
 
-  /* const deleteAlert = (eventName: string) => {
-    const index = alerts.findIndex(a => a.name === eventName)
-    console.log('INDEX TO DELETE', index)
-    // delete alerts[index]
-  } */
+  const dispatch = useAppDispatch();
+
+  const deleteAlert = (event: any) => {
+    dispatch({ type: "alerts/disableAlert", payload: event["@id"] });
+    console.log("DELETE ALERT");
+  };
 
   return (
     <MessageStyle index={index}>
       <MessageContentStyle>
-        <Text style={{
-          color: 'white',
-          fontWeight: 'bold'
-        }}>
-          {capitalize(format(new Date(event.start_date), 'PPPPp', { locale: fr }))}
+        <Text
+          style={{
+            color: "white",
+            fontWeight: "bold",
+          }}
+        >
+          {capitalize(
+            format(new Date(event.start_date), "PPPPp", { locale: fr })
+          )}
         </Text>
-        <Text style={{
-          color: 'white'
-        }}>
-          {whenEventIs(event.start_date)} {event.name.toUpperCase()} AU {event.location.name.toUpperCase()}
+        <Pressable style={styles.button} onPress={() => deleteAlert(event)}>
+          <Text style={{ color: "red", fontSize: "18" }}>×</Text>
+        </Pressable>
+        <Text
+          style={{
+            color: "white",
+          }}
+        >
+          {whenEventIs(event.start_date)} {event.name.toUpperCase()} AU{" "}
+          {event.location.name.toUpperCase()}
         </Text>
+
         {/* <FontAwesome5 onPress={deleteAlert(event.name)} style={{ position: 'absolute', right: 0, top: 10 }} name={'trash'} size={15} color="white"/> */}
       </MessageContentStyle>
-      <MessageTimestampLeft>
-      </MessageTimestampLeft>
+      <MessageTimestampLeft></MessageTimestampLeft>
     </MessageStyle>
-  )
-}
+  );
+};
 
-export default Message
+const styles = StyleSheet.create({
+  button: {
+    position: "absolute",
+    paddingVertical: 0,
+    paddingHorizontal: 0,
+    right: -3,
+    top: -10,
+  },
+});
+
+export default Message;
--- src/screens/alerts/AlertsScreen.tsx
+++ src/screens/alerts/AlertsScreen.tsx
@@ -1,27 +1,35 @@
-import TitleWithImage from '../../components/title/titleWithImage/TitleWithImage'
-import { MessageContainer } from '../../components/message/message.style'
-import HeaderIcons from '../../components/headers/HeadersIcons'
-import ScreenWrapper from '../../components/ScreenWrapper'
-import Message from '../../components/message/Message'
-import { useAppSelector } from '../../redux/hooks'
-import { ScrollView } from 'react-native'
-import { sortBy } from 'lodash'
+import TitleWithImage from "../../components/title/titleWithImage/TitleWithImage";
+import { MessageContainer } from "../../components/message/message.style";
+import HeaderIcons from "../../components/headers/HeadersIcons";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import Message from "../../components/message/Message";
+import { useAppSelector } from "../../redux/hooks";
+import { ScrollView, Button } from "react-native";
+import { sortBy } from "lodash";
 
 const AlertsScreen = () => {
-  const alerts = useAppSelector(state => state.alerts.alerts)
+  const alerts = useAppSelector((state) => state.alerts.alerts);
   return (
     <ScreenWrapper>
-      <ScrollView style={{ marginBottom: 40 }} showsVerticalScrollIndicator={false}>
-        <HeaderIcons/>
-        <TitleWithImage title={'ENVIES'} image={require('../../../assets/images/clutch/Clutch_signature.png')}/>
-        {sortBy(alerts, event => new Date(event.start_date)).map((event, index) => (
-          <MessageContainer index={index} key={index} >
-            <Message index={index} event={event}/>
-          </MessageContainer>
-        ))}
+      <ScrollView
+        style={{ marginBottom: 40 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <HeaderIcons />
+        <TitleWithImage
+          title={"ENVIES"}
+          image={require("../../../assets/images/clutch/Clutch_signature.png")}
+        />
+        {sortBy(alerts, (event) => new Date(event.start_date)).map(
+          (event, index) => (
+            <MessageContainer index={index} key={index}>
+              <Message index={index} event={event} />
+            </MessageContainer>
+          )
+        )}
       </ScrollView>
     </ScreenWrapper>
-  )
-}
+  );
+};
 
-export default AlertsScreen
+export default AlertsScreen;
